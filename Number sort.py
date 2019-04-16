bc54fa282b24fe7ae32629d6200661193969754c
Demilaw <49695333+Demilaw@users.noreply.github.com>
2019-04-16 19:07:45 +0000

    Add files via upload

---
+++ Number sort.py
@@ -0,0 +1,14 @@
+#State the author and purpose of the code
+print("This was designed by Demilade Lawal\n It is a list sorter")
+
+#Create space for neat output
+print("\n")
+
+#Declare Demilade's list of numbers
+NL = [66,22,2,5,8,40,99,3,55,35,62,49,60,80,82,46,31,21,15,13,18,9,4,81]
+
+#Use the reverse sort function to arrange in descending order
+NL.sort(reverse = True)
+
+#Print new sorted list
+print(NL)
